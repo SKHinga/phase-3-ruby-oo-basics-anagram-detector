19765742d2e82f62b868f92887e5fe64751ff8ad
Samuel Hinga <samuelhinga200@gmail.com>
2022-06-11 08:50:36 +0300

    commit tree 866dcfcb47140a04baab78f8f2ab317a9a2530dd

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+require "pry"
+class Anagram
+  attr_reader :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match (array)
+    array.select {|y| y.chars.sort == @word.chars.sort}
+  end
+end
+
+listen = Anagram.new('listen')
